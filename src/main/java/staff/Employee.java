6329b918a2559ccd52fbc489537bf57ce6a7b547
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-10 14:24:25 +0000

    Employee Class made

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,14 @@
+package staff;
+
+public class Employee {
+
+    private String name;
+    private String NI;
+    private double salary;
+
+    public Employee(String name, String NI, double salary){
+        this.name = name;
+        this.NI = NI;
+        this.salary = salary;
+    }
+}
