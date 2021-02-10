3dec7ef7b449b3e19f49d6f7470b24d1618f48b6
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-10 14:29:55 +0000

    Manager set up

---
+++ src/main/java/management/Manager.java
@@ -0,0 +1,10 @@
+package management;
+
+import staff.Employee;
+
+public class Manager extends Employee {
+    public Manager(String name, String NI, double salary){
+        super(name, NI, salary);
+
+    }
+}
